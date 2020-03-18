f5f2deb5fe77d911a2030e3af867ee74db317113
Mohamed <mohamedennoun@gmail.com>
2020-03-18 14:51:43 +0100

    Add form city and set GET and POST /ville ROUTE

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
+router.get('/ville', function(req, res, next) {
+  var params = req.query.nom_ville; 
+  res.render('ville', { villeName: params});
+  console.log("Mon parametre est : " + params);
+ 
+});
+router.post('/ville', function(req, res, next) {
+  var params = req.body.nom_ville; 
+  res.render('ville', { villeName: params});
+  console.log("Mon parametre est : " + params);
+ 
+});
 
 module.exports = router;
